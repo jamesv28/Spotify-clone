e5bb843808895ea67af5cb26d5309b717f96d3b2
James Volmert <james.volmert@gmail.com>
2025-09-03 19:46:50 -0600

    changed files to source code

--- frontend/src/providers/AuthProviders.tsx
+++ frontend/src/providers/AuthProviders.tsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import { useAuth } from "@clerk/clerk-react";
 import { axiosInstance } from "@/lib/axios";
-import { Loader } from "lucide-react";
 
-const AuthProviders = ({ children }: { children: ReactNode }) => {
+import { useAuth } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const updateApiToken = (token: string | null) => {
+  if (token)
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  else delete axiosInstance.defaults.headers.common["Authorization"];
+};
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { getToken, userId } = useAuth();
   const [loading, setLoading] = useState(true);
 
-  const updateApiToken = (token: string | null) => {
-    if (token)
-      axiosInstance.defaults.headers.common[
-        "Authorization"
-      ] = `Bearer ${token}`;
-    else delete axiosInstance.defaults.headers.common["Authorization"];
-  };
   useEffect(() => {
     const initAuth = async () => {
       try {
@@ -21,20 +21,22 @@ const AuthProviders = ({ children }: { children: ReactNode }) => {
         updateApiToken(token);
       } catch (error: unknown) {
         updateApiToken(null);
-        console.log(`Error in auth provider: ${error}`);
+        console.log("Error in auth provider", error);
       } finally {
         setLoading(false);
       }
     };
+
     initAuth();
   }, [getToken]);
+
   if (loading)
     return (
-      <div className="w-full h-screen flex items-center justify-center">
+      <div className="h-screen w-full flex items-center justify-center">
         <Loader className="size-8 text-emerald-500 animate-spin" />
       </div>
     );
-  return <div>{children}</div>;
-};
 
-export default AuthProviders;
+  return <>{children}</>;
+};
+export default AuthProvider;
