a2159ccf1f3eb053098b6570d01113c96b0d0c57
James Volmert <james.volmert@gmail.com>
2025-09-03 18:26:12 -0600

    changed code to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import { clerkMiddleware, clerkClient } from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
 import fs from "fs";
 import { createServer } from "http";
 import cron from "node-cron";
+
+import { initializeSocket } from "./lib/socket.js";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.routes.js";
@@ -23,6 +25,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 const httpServer = createServer(app);
+initializeSocket(httpServer);
 
 app.use(
   cors({
@@ -46,6 +49,19 @@ app.use(
 
 // cron jobs
 const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
